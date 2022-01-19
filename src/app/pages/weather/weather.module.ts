3cf05d6565f77ecb3dbd7de1cf050545dcfc1ec2
Lina.Lumburovska <linalumburovska20@gmail.com>
2022-01-19 14:46:25 +0100

    City card added as shared component

--- src/app/pages/weather/weather.module.ts
+++ src/app/pages/weather/weather.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { WeatherRoutingModule } from './weather-routing.module';
 import { CityService } from 'src/app/services/city.service';
 import { WeatherComponent } from './weather.component';
-import { CityCardComponent } from 'src/app/components/city-card/city-card.component';
+import { CityCardComponent } from 'src/app/shared/city-card/city-card.component';
 import { FormsModule } from '@angular/forms';
 import { WeatherFavouritesComponent } from './components/weather-favourites/weather-favourites.component';
 
